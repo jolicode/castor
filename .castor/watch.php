878b377045bf40f3f210f742df58a9431cd0de60
Joel Wurtz <jwurtz@jolicode.com>
2023-05-19 12:12:42 +0200

    Add a parallel exemple for watch

--- .castor/watch.php
+++ .castor/watch.php
@@ -2,6 +2,7 @@
 
 use Castor\Attribute\Task;
 
+use function Castor\parallel;
 use function Castor\watch;
 
 #[Task(description: 'A simple task that watch on filesystem changes')]
@@ -10,4 +11,21 @@ function fs_change()
     watch(dirname(__DIR__) . '/...', function ($name, $type) {
         echo "File {$name} has been {$type}\n";
     });
+}
+
+#[Task(description: 'A simple task that watch on filesystem changes')]
+function parralel()
+{
+    parallel(
+        function () {
+            watch(dirname(__DIR__) . '/...', function ($name, $type) {
+                echo "First: File {$name} has been {$type}\n";
+            });
+        },
+        function () {
+            watch(dirname(__DIR__) . '/...', function ($name, $type) {
+                echo "Second : File {$name} has been {$type}\n";
+            });
+        },
+    );
 }
