3ee1a275e9bf94edbe273991991af08a127bec86
Grégoire Pineau <lyrixx@lyrixx.info>
2023-05-24 20:16:53 +0200

    Rework the argument / option layer

--- .castor/args.php
+++ .castor/args.php
@@ -2,21 +2,28 @@
 
 namespace args;
 
-use Castor\Attribute\AsArg;
+use Castor\Attribute\AsArgument;
+use Castor\Attribute\AsOption;
 use Castor\Attribute\AsTask;
+use Symfony\Component\Console\Input\InputOption;
 
 use function Castor\exec;
 
-#[AsTask(description: 'This a task with arguments')]
+#[AsTask(description: 'Dumps all arguments and options, with custom configuration')]
 function args(
-    #[AsArg(description: 'This is a required argument')] string $required,
-    #[AsArg(name: 'optional', description: 'This is an optional test argument')] int $test2 = 1,
-    string|null $itsAlsoOptional = null
+    #[AsArgument(description: 'This is a required argument', suggestedValues: ['hello', 'bonjour', 'hola'])]
+    $word,
+    #[AsOption(description: 'This a an option without value in CLI', mode: InputOption::VALUE_NONE)]
+    string $force,
+    #[AsArgument(name: 'array-of-people', description: 'This is an optional array argument')]
+    array $argument2 = ['world', 'PHP community'],
+    #[AsOption(description: 'This with an option with an optional value')]
+    string $option = 'default value',
 ) {
-    exec(['echo', $required, $test2, $itsAlsoOptional]);
+    var_dump(\func_get_args());
 }
 
-#[AsTask(description: 'This a another task with arguments, work without attributes')]
+#[AsTask(description: 'Dumps all arguments and options, without configuration')]
 function another_args(
     string $required,
     int $test2 = 1
--- src/Attribute/AsOption.php
+++ src/Attribute/AsOption.php
@@ -3,15 +3,18 @@
 namespace Castor\Attribute;
 
 #[\Attribute(\Attribute::TARGET_PARAMETER)]
-class AsArg
+class AsOption
 {
     /**
      * @param string|array<string>|null $shortcut
+     * @param array<string>             $suggestedValues
      */
     public function __construct(
         public string|null $name = null,
-        public string|null $description = null,
         public string|array|null $shortcut = null,
+        public int|null $mode = null,
+        public string $description = '',
+        public array $suggestedValues = [],
     ) {
     }
 }
