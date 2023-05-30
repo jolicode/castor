b5c226590adb296a7a7db422f908785d3a81ab57
Grégoire Pineau <lyrixx@lyrixx.info>
2023-05-30 14:38:38 +0200

    Better watch example

--- examples/watch.php
+++ examples/watch.php
@@ -10,6 +10,7 @@ use function Castor\watch;
 #[AsTask(description: 'Watches on filesystem changes')]
 function fs_change()
 {
+    echo "Try editing a file\n";
     watch(\dirname(__DIR__) . '/...', function ($name, $type) {
         echo "File {$name} has been {$type}\n";
     });
@@ -31,13 +32,25 @@ function parallel_change()
 {
     parallel(
         function () {
+            for ($i = 1; $i <= 10; ++$i) {
+                echo "[app] Writing hello-{$i}.txt\n";
+                file_put_contents("hello-{$i}.txt", "Hello {$i}\n", \FILE_APPEND);
+                echo "[app] Deleting hello-{$i}.txt\n";
+                unlink("hello-{$i}.txt");
+                if (\Fiber::getCurrent()) {
+                    \Fiber::suspend();
+                }
+                usleep(500_000);
+            }
+        },
+        function () {
             watch(\dirname(__DIR__) . '/...', function ($name, $type) {
-                echo "First: File {$name} has been {$type}\n";
+                echo "[watcher:A] File {$name} has been {$type}\n";
             });
         },
         function () {
             watch(\dirname(__DIR__) . '/...', function ($name, $type) {
-                echo "Second : File {$name} has been {$type}\n";
+                echo "[watcher:B] Second : File {$name} has been {$type}\n";
             });
         },
     );
