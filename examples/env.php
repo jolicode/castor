d8e6e2df53dae5f5b2f1d71e0877a615d1cf10fa
Grégoire Pineau <lyrixx@lyrixx.info>
2023-06-05 14:06:01 +0200

    Do not talk about Context, when alternative exist

--- examples/env.php
+++ examples/env.php
@@ -3,15 +3,13 @@
 namespace env;
 
 use Castor\Attribute\AsTask;
-use Castor\Context;
 
 use function Castor\run;
 
 #[AsTask(description: 'Display environment variables')]
-function env(Context $context)
+function env()
 {
-    $context = $context->withEnvironment([
+    run('echo \"$FOO\"', environment: [
         'FOO' => 'toto',
     ]);
-    run('echo \"$FOO\"', context: $context);
 }
