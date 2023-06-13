7339a1264bba79723d593ad1a4b9b961c7e4bee0
Loïck Piera <pyrech@gmail.com>
2023-06-13 10:22:29 +0200

    Fix initial project after arguments refactoring

--- src/Console/Command/CastorFileNotFoundCommand.php
+++ src/Console/Command/CastorFileNotFoundCommand.php
@@ -52,14 +52,15 @@ class CastorFileNotFoundCommand extends SingleCommandApplication
             use Castor\Attribute\AsTask;
             use Symfony\Component\Console\Style\SymfonyStyle;
 
+            use function Castor\io;
             use function Castor\run;
 
             #[AsTask(description: 'Welcome to Castor!')]
-            function hello(SymfonyStyle $io): void
+            function hello(): void
             {
                 $currentUser = trim(run('whoami', quiet: true)->getOutput());
 
-                $io->title(sprintf('Hello %s!', $currentUser));
+                io()->title(sprintf('Hello %s!', $currentUser));
             }
             PHP
         );
