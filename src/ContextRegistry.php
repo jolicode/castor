9b9393530e048f2ca1dba1f37cd74fee586e846e
Grégoire Pineau <lyrixx@lyrixx.info>
2023-06-23 18:31:09 +0200

    Code gromming

--- src/ContextRegistry.php
+++ src/ContextRegistry.php
@@ -7,22 +7,22 @@ class ContextRegistry
 {
     /** @var array<string, ContextDescriptor> */
     private array $descriptors = [];
-    private ?ContextDescriptor $default = null;
+    private ?string $default = null;
 
     public function add(ContextDescriptor $descriptor): void
     {
         $name = $descriptor->contextAttribute->name;
         if (\array_key_exists($name, $this->descriptors)) {
-            throw new \RuntimeException(sprintf('You cannot defined two context with the same name "%s". There is one defined in "%s" and another in "%s".', $name, $this->descriptors[$name]->function->getName(), $descriptor->function->getName()));
+            throw new \RuntimeException(sprintf('You cannot defined two context with the same name "%s". There is one defined in "%s" and another in "%s".', $name, $this->describeFunction($this->descriptors[$name]->function), $this->describeFunction($descriptor->function)));
         }
 
         $this->descriptors[$name] = $descriptor;
 
         if ($descriptor->contextAttribute->default) {
             if ($this->default) {
-                throw new \RuntimeException(sprintf('You cannot set multiple "default: true" context. There is one defined in "%s" and another in "%s".', $this->default->function->getName(), $descriptor->function->getName()));
+                throw new \RuntimeException(sprintf('You cannot set multiple "default: true" context. There is one defined in "%s" and another in "%s".', $this->default, $this->describeFunction($descriptor->function)));
             }
-            $this->default = $descriptor;
+            $this->default = $name;
         }
     }
 
@@ -37,20 +37,24 @@ class ContextRegistry
         }
 
         if (1 < \count($this->descriptors)) {
-            throw new \RuntimeException(sprintf('Since there are multiple contexts "%s", you must set a "default: true" context.', implode('", "', array_keys($this->descriptors))));
+            throw new \RuntimeException(sprintf('Since there are multiple contexts "%s", you must set a "default: true" context.', implode('", "', $this->getNames())));
         }
 
-        $this->default = reset($this->descriptors);
+        $this->default = array_key_first($this->descriptors);
     }
 
-    public function getDefault(): ContextDescriptor
+    public function getDefault(): string
     {
-        return $this->default ?? throw new \LogicException('Default descriptor not set yet.');
+        return $this->default ?? throw new \LogicException('Default context not set yet.');
     }
 
-    public function get(string $name): ContextDescriptor
+    public function get(string $name): Context
     {
-        return $this->descriptors[$name] ?? throw new \RuntimeException(sprintf('Descriptor "%s" not found.', $name));
+        if (!\array_key_exists($name, $this->descriptors)) {
+            throw new \RuntimeException(sprintf('Context "%s" not found.', $name));
+        }
+
+        return $this->descriptors[$name]->function->invoke();
     }
 
     /**
@@ -58,6 +62,18 @@ class ContextRegistry
      */
     public function getNames(): array
     {
-        return array_keys($this->descriptors);
+        $names = array_keys($this->descriptors);
+        sort($names);
+
+        return $names;
+    }
+
+    private function describeFunction(\ReflectionFunction $function): string
+    {
+        $name = $function->getName();
+        $shortFilename = str_replace(PathHelper::getRoot() . '/', '', (string) $function->getFileName());
+        $location = sprintf('%s:%d', $shortFilename, $function->getStartLine());
+
+        return sprintf('%s@%s', $name, $location);
     }
 }
