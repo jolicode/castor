0337574d5299f580c85c2a6589c86b3588f89e10
Theo D <theo.d02290@gmail.com>
2024-01-04 22:34:49 +0100

    fix: Add auto installation of vendor for tools (phpstan/php-cs-fixer)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 # Install composer dependencies
 composer install --no-interaction --no-progress --no-suggest
 
+# Install tools dependencies
+cd tools/php-cs-fixer && composer install --no-interaction --no-progress --no-suggest && cd ../..
+cd tools/phpstan && composer install --no-interaction --no-progress --no-suggest && cd ../..
+
 # Keep container running
 tail -f /dev/null
