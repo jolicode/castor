99d3c92adb743f2265f688b9da94aad11b26c940
Théo D <72203064+TheoD02@users.noreply.github.com>
2025-01-28 17:54:21 +0100

    fix: --context option ignored when another option presence (#593) (#594)
    Co-authored-by: Theo D <theo.dulieu@wyzgroup.com>

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,6 +11,7 @@ use Castor\Event\AfterBootEvent;
 use Castor\Event\BeforeBootEvent;
 use Castor\Event\FunctionsResolvedEvent;
 use Castor\Exception\CouldNotFindEntrypointException;
+use Castor\Factory\TaskCommandFactory;
 use Castor\Function\FunctionLoader;
 use Castor\Function\FunctionResolver;
 use Castor\Helper\PlatformHelper;
@@ -46,6 +47,7 @@ final class Kernel
         private readonly FunctionResolver $functionResolver,
         private readonly FunctionLoader $functionLoader,
         private readonly ContextRegistry $contextRegistry,
+        private readonly TaskCommandFactory $taskCommandFactory,
     ) {
     }
 
@@ -122,6 +124,22 @@ final class Kernel
         }
 
         $this->functionLoader->loadListeners($descriptorsCollection->listenerDescriptors);
+
+        /**
+         * Resolve the current task command before loading and configuring contexts.
+         * This is necessary because the task command can add options to the application.
+         * If we do not add these options before configuring the context,
+         * binding the input to the application definition will fail.
+         *
+         * @see https://github.com/jolicode/castor/issues/593
+         */
+        $taskCommand = $this->resolveCurrentTaskCommand($descriptorsCollection, $input);
+
+        $taskDefinition = $taskCommand?->getDefinition();
+
+        if (null !== $taskDefinition) {
+            $this->application->getDefinition()->addOptions($taskDefinition->getOptions());
+        }
 
         // Must load contexts before tasks, because tasks can be disabled
         // depending on the context. And it must be before executing
@@ -242,5 +260,23 @@ final class Kernel
         }
 
         $this->contextRegistry->setCurrentContext($context->withName($input->getOption('context')));
+    }
+
+    private function resolveCurrentTaskCommand(DescriptorsCollection $descriptorsCollection, InputInterface $input): ?Console\Command\TaskCommand
+    {
+        $taskCommand = null;
+
+        foreach ($descriptorsCollection->taskDescriptors as $taskDescriptor) {
+            $namespace = $taskDescriptor->taskAttribute->namespace;
+            $taskName = $taskDescriptor->taskAttribute->name;
+            $fullTaskName = $namespace ? $namespace . ':' . $taskName : $taskName;
+            if ($fullTaskName === $input->getFirstArgument()) {
+                $taskCommand = $this->taskCommandFactory->createTask($taskDescriptor);
+
+                break;
+            }
+        }
+
+        return $taskCommand;
     }
 }
