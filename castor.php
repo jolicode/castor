24b648d05aa3c575ab4bad79909c54b46c84eb0c
Joel Wurtz <jwurtz@jolicode.com>
2025-03-05 18:10:18 +0100

    chore(tools): use castor php qa for cs fixer and phpstan

--- castor.php
+++ castor.php
@@ -10,9 +10,8 @@ use function Castor\run;
 use function Castor\with;
 
 import(__DIR__ . '/examples');
+import(__DIR__ . '/.castor/qa.php');
 import(__DIR__ . '/tools/mkdocs/castor.php');
-import(__DIR__ . '/tools/php-cs-fixer/castor.php');
-import(__DIR__ . '/tools/phpstan/castor.php');
 import(__DIR__ . '/tools/static/castor.php');
 
 mount(__DIR__ . '/tools/phar');
