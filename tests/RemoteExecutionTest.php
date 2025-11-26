07e7eb690b3fa0300dd212946e4fd8e4faf15f74
Loïck Piera <pyrech@gmail.com>
2025-11-26 11:43:14 +0100

    Fix tests

--- tests/RemoteExecutionTest.php
+++ tests/RemoteExecutionTest.php
@@ -32,13 +32,13 @@ class RemoteExecutionTest extends TaskTestCase
 
     public function testVersion(): void
     {
-        $process = $this->runTask(['execute', 'composer/composer:2.7@composer', '-v']);
+        $process = $this->runTask(['execute', 'composer/composer:2.9@composer', '-v']);
 
         if (0 !== $process->getExitCode()) {
             throw new ProcessFailedException($process);
         }
 
-        $this->assertStringContainsString('Installing composer/composer (2.7', $process->getErrorOutput());
-        $this->assertStringContainsString('Composer version 2.7', $process->getOutput());
+        $this->assertStringContainsString('Installing composer/composer (2.9', $process->getErrorOutput());
+        $this->assertStringContainsString('Composer version 2.9', $process->getOutput());
     }
 }
